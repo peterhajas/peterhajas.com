59ae989071f8121ceb5de2debcd49da6f8d29b75
Peter Hajas <peterhajas@gmail.com>
2020-01-20 14:26:17 -0800

    More progress on python buildscript: - use a class to generate stuff - move many global functions into this class

--- build.py
+++ build.py
@@ -20,57 +20,59 @@ class MarkdownFile:
     title = None
     # The date associated with this file, or None if it has none
     date = None
+    # The contents associated with this file
+    contents = ""
 
-# Given a Markdown file, returns the metadata for it or None if none exists
-def metadata_for_markdown_file_contents(contents):
-    lines = contents.split("\n")
-    # If this markdown file doesn't have 5 or more lines, it is missing its
-    # metadata
-    if len(lines) < 5:
-        return None
-    # If the second line isn't an HTML comment, this has no metadata
-    if lines[1] != "<!--":
-        return None
-    # If the fifth line isn't an ending comment, this has no metadata
-    if lines[4] != "-->":
-        return None
-    # OK, so now lines[2] has our title and lines[3] has our date string
-    title = lines[2]
-    date_string = lines[3]
-    date = datetime.strptime(date_string, "%Y%m%d %H:%M")
-    return { "date" : date, "title" : title }
+    def metadata(self):
+        lines = self.contents.split("\n")
+        # If this markdown file doesn't have 5 or more lines, it is missing its
+        # metadata
+        if len(lines) < 5:
+            return None
+        # If the second line isn't an HTML comment, this has no metadata
+        if lines[1] != "<!--":
+            return None
+        # If the fifth line isn't an ending comment, this has no metadata
+        if lines[4] != "-->":
+            return None
+        # OK, so now lines[2] has our title and lines[3] has our date string
+        title = lines[2]
+        date_string = lines[3]
+        date = datetime.strptime(date_string, "%Y%m%d %H:%M")
+        return { "date" : date, "title" : title }
 
-# Returns a pretty date from a datetime date
-def pretty_date(date):
-    return date.strftime("%B %e, %Y") 
+    def __init__(self, contents):
+        self.contents = contents
+        metadata = self.metadata()
+        if metadata != None:
+            self.title = metadata["title"]
+            self.date = metadata["date"]
 
-# Processes the file 
-def process_markdown(path, dated):
+    # A pretty-formatted date for self
+    def pretty_date(self):
+        return self.date.strftime("%B %e, %Y") 
 
-for path in all_markdown_paths:
-    file_contents = path.read_text()
-    # Grab the "raw" html
-    file_html = markdown.markdown(file_contents, extensions=markdown_extensions)
-    # Grab the metadata
-    metadata = metadata_for_markdown_file_contents(file_contents)
+    # Just the HTML for this entry
+    def html(self):
+        return markdown.markdown(self.contents, extensions=markdown_extensions)
 
-    # The page html is the file html, but with:
-    # - before before it
-    # - after after it
-    # - the date and title replaced (if the page has one)
-    page_html = before_html + file_html + after_html
-    if metadata != None:
-        page_html = page_html.replace("TITLE_HERE", metadata["title"])
-        date_pretty = pretty_date(metadata["date"])
-        page_html = page_html.replace("DATE_HERE", date_pretty)
-    else:
-        page_html = page_html.replace("TITLE_HERE", "")
-        page_html = page_html.replace("DATE_HERE", "")
-
-    print(page_html)
-    # quit()
-
-
+    # HTML that can stand alone
+    def page_html(self):
+        # The page html is the html, but with:
+        # - before before it
+        # - after after it
+        # - the date and title replaced (if the page has one)
+        html = self.html()
+        page_html = before_html + html + after_html
+        if self.title != None:
+            page_html = page_html.replace("TITLE_HERE", self.title)
+        else:
+            page_html = page_html.replace("TITLE_HERE", "")
+        if self.date != None:
+            page_html = page_html.replace("DATE_HERE", self.pretty_date())
+        else:
+            page_html = page_html.replace("DATE_HERE", "")
+        return page_html
 
 # Delete the current path
 # pathlib will only delete empty directories, so we use shutil
@@ -81,6 +83,10 @@ output_path.mkdir(parents=True, exist_ok=True)
 
 # Process the site. We'll look for all the markdown files in our tree
 all_markdown_paths = sorted(Path().rglob("*.md"))
+for path in all_markdown_paths:
+    file_contents = path.read_text()
+    markdown_file = MarkdownFile(file_contents)
+    print(markdown_file.page_html())
 
 # We want to use these to:
 # - build the index (sorted by date, included in the file)
