afaac25de0b585522fe4088f2dc2bc51614c1797
Peter Hajas <peterhajas@gmail.com>
2020-01-20 16:32:23 -0800

    Some version of RSS exporting

--- build.py
+++ build.py
@@ -19,8 +19,13 @@ markdown_extensions = ["tables", "smarty"]
 before_html = Path("before.html").read_text()
 after_html = Path("after.html").read_text()
 
+# The rss_before.xml and rss_after.xml files
+rss_before_xml = Path("rss_before.xml").read_text()
+rss_after_xml = Path("rss_after.xml").read_text()
+
 output_path = Path(output_path_string)
 index_output_path = output_path.joinpath("index.html")
+rss_output_path = output_path.joinpath("rss.xml")
 
 class MarkdownFile:
     # The title associated with this file, or None if it has none
@@ -70,6 +75,10 @@ class MarkdownFile:
     def pretty_date(self):
         return self.date.strftime("%B %e, %Y") 
 
+    # An RSS-formatted date for self
+    def rss_date(self):
+        return self.date.strftime("%a, %d %b %Y %H:%M:%S %Z")
+
     # Just the HTML for this entry
     def html(self):
         return markdown.markdown(self.contents, extensions=markdown_extensions)
@@ -98,10 +107,20 @@ class MarkdownFile:
         relative = self.export_path.relative_to(output_path)
         return relative
 
+    # The RSS item for this page
+    def rss_item(self):
+        return """<item>
+        <title>{}</title>
+        <guid>{}</guid>
+        <pubDate>{}</pubDate>
+        <description><![CDATA[
+        {}
+        ]]></description>
+        </item>""".format(self.title, self.rendered_path(), self.rss_date(), self.html())
+
     # Renders the file to its output location
     def render(self):
         output_path = self.export_path
-        print("rendering to {}".format(output_path))
         output_path.parent.mkdir(parents=True, exist_ok=True)
         output_path.write_text(self.page_html())
 
@@ -153,14 +172,22 @@ sorted_dated_markdown_files = sorted(dated_markdown_files, key=lambda entry: ent
 sorted_dated_markdown_files.reverse()
 
 index_markdown_contents = ""
+rss_contents = rss_before_xml
 # Build the index and rss files
 for entry in sorted_dated_markdown_files:
     # Index: add in title and date
     index_markdown_contents += "<div class='title'><a href='{}'>{}</a></div>\n".format(entry.rendered_path(), entry.title)
     index_markdown_contents += "<div class='date'>{}</div>\n".format(entry.pretty_date())
+
     # Index: add in contents
     index_markdown_contents += entry.html()
     index_markdown_contents += "\n"
 
+    # RSS: add in item RSS contents
+    rss_contents += entry.rss_item()
+
 index_markdown = MarkdownFile(export_path=index_output_path, contents=index_markdown_contents)
 index_markdown.render()
+
+rss_contents += rss_after_xml
+rss_output_path.write_text(rss_contents)
