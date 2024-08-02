05aaee75e7c983c28e8c5dd0c9306ba4dab7b488
Peter Hajas <peter@haj.as>
2024-08-02 13:08:50 -0600

    add new wiki path

--- configs/tiddlywiki_listener/entrypoint.bash
+++ configs/tiddlywiki_listener/entrypoint.bash
@@ -8,7 +8,7 @@ rm /out/*
 rm -r /out/*
 
 echo "EXPORTING"
-$TIDDLYWIKI --load /wiki/wiki.html --output /tmp/ --render '.' public.json 'text/plain' '$:/core/templates/exporters/JsonFile' 'exportFilter' '[tag[Public]]:or[tag[phajas]]:or[prefix[$:/phajas]]:except[tag[Private]]'
+$TIDDLYWIKI --load /wiki/phajas-wiki.html --output /tmp/ --render '.' public.json 'text/plain' '$:/core/templates/exporters/JsonFile' 'exportFilter' '[tag[Public]]:or[tag[phajas]]:or[prefix[$:/phajas]]:except[tag[Private]]'
 
 echo "STRIPPING PUBLIC TAGS"
 /tiddlywiki_strip_public_tag /tmp/public.json
@@ -32,7 +32,7 @@ mv /out/static/* /out/
 rm -r /out/static
 
 echo "GRABBING PUBLIC EXTERNAL ASSETS"
-$TIDDLYWIKI --load /wiki/wiki.html --output /tmp/ --render '.' external_assets.json 'text/plain' '$:/core/templates/exporters/JsonFile' 'exportFilter' '[tag[Public]has[_canonical_uri]]'
+$TIDDLYWIKI --load /wiki/phajas-wiki.html --output /tmp/ --render '.' external_assets.json 'text/plain' '$:/core/templates/exporters/JsonFile' 'exportFilter' '[tag[Public]has[_canonical_uri]]'
 
 cat /tmp/external_assets.json | jq -r '.[] | ._canonical_uri' | sed 's/%20/ /g' | while read -r uri; do
   dir_path="/out/$(dirname "$uri")"
@@ -41,7 +41,7 @@ cat /tmp/external_assets.json | jq -r '.[] | ._canonical_uri' | sed 's/%20/ /g' 
 done
 
 echo "GENERATING RSS FEED"
-$TIDDLYWIKI --load /wiki/wiki.html --render "[[$:/plugins/sq/feeds/templates/rss]]" "feed.xml" "text/plain" "$:/core/templates/wikified-tiddler"
+$TIDDLYWIKI --load /wiki/phajas-wiki.html --render "[[$:/plugins/sq/feeds/templates/rss]]" "feed.xml" "text/plain" "$:/core/templates/wikified-tiddler"
 mv output/feed.xml /out/feed.xml
 
 echo "ALL DONE"
