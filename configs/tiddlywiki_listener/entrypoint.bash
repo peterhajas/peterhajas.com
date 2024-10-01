ea6a75aac492a9cd9adfc2821656937d957dcc5e
Peter Hajas <peter@haj.as>
2024-10-01 05:37:00 -0600

    replace tiddlywiki contents in a less atomic fashion

--- configs/tiddlywiki_listener/entrypoint.bash
+++ configs/tiddlywiki_listener/entrypoint.bash
@@ -3,8 +3,10 @@
 echo "PREPARING"
 # TIDDLYWIKI="tiddlywiki +plugins/tiddlywiki/markdown --verbose"
 TIDDLYWIKI="tiddlywiki +plugins/tiddlywiki/markdown"
+OUTPUT=/out/NEW
 
 mkdir /tmp/input
+mkdir -p $OUTPUT
 git --git-dir=/wiki archive master | tar -x -C /tmp/input/
 
 echo "CLEANING UP"
@@ -18,35 +20,39 @@ echo "STRIPPING PUBLIC TAGS"
 /tiddlywiki_strip_public_tag /tmp/public.json
 
 echo "IMPORTING INTO BASE WIKI"
-$TIDDLYWIKI --load /base.html --import /tmp/public.json application/json --output /tmp/ --render "\$:/core/save/all" "/out/index.html" "text/plain"
+$TIDDLYWIKI --load /base.html --import /tmp/public.json application/json --output /tmp/ --render "\$:/core/save/all" "$OUTPUT/index.html" "text/plain"
 
 echo "GENERATING STATIC VARIANT"
-mkdir -p /out/static
+mkdir -p $OUTPUT/static
 
 # HTML representations of individual tiddlers
-$TIDDLYWIKI --load /out/index.html --rendertiddlers '[!is[system]]' $:/core/templates/static.tiddler.html /out/static text/plain
+$TIDDLYWIKI --load $OUTPUT/index.html --rendertiddlers '[!is[system]]' $:/core/templates/static.tiddler.html $OUTPUT/static text/plain
 
 # CSS
-$TIDDLYWIKI --load /out/index.html --rendertiddler $:/core/templates/static.template.css /out/static/static.css text/plain
+$TIDDLYWIKI --load $OUTPUT/index.html --rendertiddler $:/core/templates/static.template.css $OUTPUT/static/static.css text/plain
 
 # TiddlyWiki likes to take ownership over the directory it operates in, which
 # is fine. To help us get the behavior we want with images, move things up a
-# level so they're all in `/out/` (and nix `/out/static`).
-mv /out/static/* /out/
-rm -r /out/static
+# level so they're all in `$OUTPUT/` (and nix `$OUTPUT/static`).
+mv $OUTPUT/static/* $OUTPUT/
+rm -r $OUTPUT/static
 
 echo "GRABBING PUBLIC EXTERNAL ASSETS"
 $TIDDLYWIKI --load /tmp/input/phajas-wiki.html --output /tmp/ --render '.' external_assets.json 'text/plain' '$:/core/templates/exporters/JsonFile' 'exportFilter' '[tag[Public]has[_canonical_uri]]'
 
 cat /tmp/external_assets.json | jq -r '.[] | ._canonical_uri' | sed 's/%20/ /g' | while read -r uri; do
-  dir_path="/out/$(dirname "$uri")"
+  dir_path="$OUTPUT/$(dirname "$uri")"
   mkdir -p "$dir_path"
   cp "/tmp/input/$uri" "$dir_path/"
 done
 
 echo "GENERATING RSS FEED"
 $TIDDLYWIKI --load /tmp/input/phajas-wiki.html --render "[[$:/plugins/sq/feeds/templates/rss]]" "feed.xml" "text/plain" "$:/core/templates/wikified-tiddler"
-mv output/feed.xml /out/feed.xml
+mv output/feed.xml $OUTPUT/feed.xml
+
+echo "CLEANING UP"
+rm /out/*
+mv $OUTPUT/* /out/
 
 echo "ALL DONE"
 
