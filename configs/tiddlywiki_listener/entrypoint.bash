185cb6a368b0acac26bad33ce34958b4a705be83
Peter Hajas <peter@haj.as>
2024-11-20 21:07:13 -0700

    fix up updating wiki

--- configs/tiddlywiki_listener/entrypoint.bash
+++ configs/tiddlywiki_listener/entrypoint.bash
@@ -1,41 +1,41 @@
 #!/bin/bash
 
 echo "PREPARING"
-# TIDDLYWIKI="tiddlywiki +plugins/tiddlywiki/markdown --verbose"
-TIDDLYWIKI="tiddlywiki +plugins/tiddlywiki/markdown"
-OUTPUT=/out/NEW
+TIDDLYWIKI="tiddlywiki"
+
+OUTPUT=/tmp/NEW
 BASE=/out/BASE
+
+rm -r $BASE 2>>/dev/null
 
 mkdir /tmp/input
 mkdir -p $OUTPUT
 mkdir -p $BASE
 git --git-dir=/wiki archive master | tar -x -C /tmp/input/
 
-# echo "CLEANING UP"
-# rm /out/*
-# rm -r /out/*
-
-echo "EXPORTING"
 $TIDDLYWIKI --load /tmp/input/phajas-wiki.html --output /tmp/ --render '.' public.json 'text/plain' '$:/core/templates/exporters/JsonFile' 'exportFilter' '[tag[Public]]:or[tag[phajas]]:or[prefix[$:/phajas]]:except[tag[Private]]'
 
-echo "STRIPPING PUBLIC TAGS"
 /tiddlywiki_strip_public_tag /tmp/public.json
 
-echo "APPLYING PUBLIC FIELDs"
 /tiddlywiki_apply_public_fields /tmp/public.json
 
-echo "CREAETING NEW BASE WIKI"
-$TIDDLYWIKI $BASE --init empty
-$TIDDLYWIKI $BASE --build index
+$TIDDLYWIKI /out/BASE --init empty
+$TIDDLYWIKI /out/BASE --build index
 
-echo "IMPORTING INTO BASE WIKI"
-$TIDDLYWIKI --load $BASE/output/index.html --import /tmp/public.json application/json --output /tmp/ --render "\$:/core/save/all" "$OUTPUT/index.html" "text/plain"
+ls /out/BASE
 
-echo "GENERATING STATIC VARIANT"
+mv $BASE/output/index.html $BASE/
+
+ls $BASE
+
+$TIDDLYWIKI --load $BASE/index.html --import /tmp/public.json application/json --output /tmp/ --render "\$:/core/save/all" "$OUTPUT/index.html" "text/plain"
+
+ls $OUTPUT
+
 mkdir -p $OUTPUT/static
 
 # HTML representations of individual tiddlers
-$TIDDLYWIKI --load $OUTPUT/index.html --rendertiddlers '[!is[system]]' $:/core/templates/static.tiddler.html $OUTPUT/static text/plain
+$TIDDLYWIKI +plugins/tiddlywiki/markdown --verbose --load $OUTPUT/index.html --rendertiddlers '[!is[system]]' $:/core/templates/static.tiddler.html $OUTPUT/static text/plain
 
 # CSS
 $TIDDLYWIKI --load $OUTPUT/index.html --rendertiddler $:/core/templates/static.template.css $OUTPUT/static/static.css text/plain
@@ -44,10 +44,9 @@ $TIDDLYWIKI --load $OUTPUT/index.html --rendertiddler $:/core/templates/static.t
 # is fine. To help us get the behavior we want with images, move things up a
 # level so they're all in `$OUTPUT/` (and nix `$OUTPUT/static`).
 mv $OUTPUT/static/* $OUTPUT/
-rm -r $OUTPUT/static
+# rm -r $OUTPUT/static
 
-echo "GRABBING PUBLIC EXTERNAL ASSETS"
-$TIDDLYWIKI --load /tmp/input/phajas-wiki.html --output /tmp/ --render '.' external_assets.json 'text/plain' '$:/core/templates/exporters/JsonFile' 'exportFilter' '[tag[Public]has[_canonical_uri]]'
+$TIDDLYWIKI  --load /tmp/input/phajas-wiki.html --output /tmp/ --render '.' external_assets.json 'text/plain' '$:/core/templates/exporters/JsonFile' 'exportFilter' '[tag[Public]has[_canonical_uri]]'
 
 cat /tmp/external_assets.json | jq -r '.[] | ._canonical_uri' | sed 's/%20/ /g' | while read -r uri; do
   dir_path="$OUTPUT/$(dirname "$uri")"
@@ -55,13 +54,11 @@ cat /tmp/external_assets.json | jq -r '.[] | ._canonical_uri' | sed 's/%20/ /g' 
   cp "/tmp/input/$uri" "$dir_path/"
 done
 
-echo "GENERATING RSS FEED"
-$TIDDLYWIKI --load /tmp/input/phajas-wiki.html --render "[[$:/plugins/sq/feeds/templates/rss]]" "rss.xml" "text/plain" "$:/core/templates/wikified-tiddler"
+$TIDDLYWIKI --load /tmp/input/phajas-wiki.html --render "[[$:/plugins/sq/feeds/templates/rss]]" "rss.xml" "text/plain" "$:/core/templates/wikified-tiddler" +plugins/tiddlywiki/markdown --verbose
 mv output/rss.xml $OUTPUT/rss.xml
 
-echo "CLEANING UP"
 rm /out/*
-mv $OUTPUT/* /out/
-
-echo "ALL DONE"
+rm -r /out/*
+rm -r /out/BASE
+mv --force /tmp/NEW/* /out/
 
