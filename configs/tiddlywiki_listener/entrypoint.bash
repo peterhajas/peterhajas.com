3880e1c78a24d3dfe3da933436c7e80c2dfadfba
Peter Hajas <peter@haj.as>
2024-12-28 09:57:48 -0700

    fix the tiddlywiki_listener for assets, get a debugging version working

--- configs/tiddlywiki_listener/entrypoint.bash
+++ configs/tiddlywiki_listener/entrypoint.bash
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-echo "PREPARING"
+# Priors - set by podman env or otherwise conventional
+# Uncomment for debugging this script
+# TMP=~/scratch/wikiscript/tmp
+# OUT=~/scratch/wikiscript/out
+# WIKI_SRC=~/scratch/wikiscript/wiki
+
+# Comment for debugging this script
+TMP=/tmp
+OUT=/out
+WIKI_SRC=/wiki
+
 TIDDLYWIKI="tiddlywiki"
 
-OUTPUT=/tmp/NEW
-BASE=/out/BASE
+OUTPUT=$OUT/NEW
+BASE=$OUT/BASE
 
 rm -r $BASE 2>>/dev/null
 
-mkdir /tmp/input
+mkdir $TMP/input
 mkdir -p $OUTPUT
 mkdir -p $BASE
-git --git-dir=/wiki archive master | tar -x -C /tmp/input/
+git --git-dir="$WIKI_SRC" archive master | tar -x -C $TMP/input/
 
 # clean up old manifest
 while IFS= read -r filepath
@@ -25,29 +35,26 @@ do
     fi
     
     # Remove file, count failures
-    if ! rm "$filepath" 2>/dev/null; then
-        echo "Failed to remove: $filepath"
-        ((FAILED_REMOVALS++))
-    fi
-done < "/out/manifest.txt"
+    rm "$filepath"
+    rm -r "$filepath"
+done < "$OUT/wiki_manifest.txt"
 
-$TIDDLYWIKI --load /tmp/input/phajas-wiki.html --output /tmp/ --render '.' public.json 'text/plain' '$:/core/templates/exporters/JsonFile' 'exportFilter' '[tag[Public]]:or[tag[phajas]]:or[prefix[$:/phajas]]:except[tag[Private]]' +plugins/tiddlywiki/markdown
+$TIDDLYWIKI --load $TMP/input/phajas-wiki.html --output $TMP/ --render '.' public.json 'text/plain' '$:/core/templates/exporters/JsonFile' 'exportFilter' '[tag[Public]]:or[tag[phajas]]:or[prefix[$:/phajas]]:except[tag[Private]]' +plugins/tiddlywiki/markdown
 
-/tiddlywiki_strip_public_tag /tmp/public.json
+./tiddlywiki_strip_public_tag $TMP/public.json
+./tiddlywiki_apply_public_fields $TMP/public.json
 
-/tiddlywiki_apply_public_fields /tmp/public.json
-
-$TIDDLYWIKI /out/BASE --init empty 
-$TIDDLYWIKI +plugins/tiddlywiki/markdown /out/BASE --build index
+$TIDDLYWIKI $BASE --init empty 
+$TIDDLYWIKI +plugins/tiddlywiki/markdown $BASE --build index
 
 mv $BASE/output/index.html $BASE/
 
-$TIDDLYWIKI --load $BASE/index.html --import /tmp/public.json application/json --output /tmp/ --render "\$:/core/save/all" "$OUTPUT/index.html" "text/plain"
+$TIDDLYWIKI --load $BASE/index.html --import $TMP/public.json application/json --output $TMP/ --render "\$:/core/save/all" "$OUTPUT/index.html" "text/plain"
 
 mkdir -p $OUTPUT/static
 
 # HTML representations of individual tiddlers
-$TIDDLYWIKI +plugins/tiddlywiki/markdown --verbose --load $OUTPUT/index.html --rendertiddlers '[!is[system]]' $:/core/templates/static.tiddler.html $OUTPUT/static text/plain
+$TIDDLYWIKI +plugins/tiddlywiki/markdown --load $OUTPUT/index.html --rendertiddlers '[!is[system]]' $:/core/templates/static.tiddler.html $OUTPUT/static text/plain
 
 # CSS
 $TIDDLYWIKI --load $OUTPUT/index.html --rendertiddler $:/core/templates/static.template.css $OUTPUT/static/static.css text/plain
@@ -56,22 +63,25 @@ $TIDDLYWIKI --load $OUTPUT/index.html --rendertiddler $:/core/templates/static.t
 # is fine. To help us get the behavior we want with images, move things up a
 # level so they're all in `$OUTPUT/` (and nix `$OUTPUT/static`).
 mv $OUTPUT/static/* $OUTPUT/
-# rm -r $OUTPUT/static
+rm -r $OUTPUT/static
 
-$TIDDLYWIKI  --load /tmp/input/phajas-wiki.html --output /tmp/ --render '.' external_assets.json 'text/plain' '$:/core/templates/exporters/JsonFile' 'exportFilter' '[tag[Public]has[_canonical_uri]]'
+$TIDDLYWIKI  --load $TMP/input/phajas-wiki.html --output $TMP/ --render '.' external_assets.json 'text/plain' '$:/core/templates/exporters/JsonFile' 'exportFilter' '[tag[Public]has[_canonical_uri]]'
 
-cat /tmp/external_assets.json | jq -r '.[] | ._canonical_uri' | sed 's/%20/ /g' | while read -r uri; do
+cat $TMP/external_assets.json | jq -r '.[] | ._canonical_uri' | sed 's/%20/ /g' | while read -r uri; do
   dir_path="$OUTPUT/$(dirname "$uri")"
   mkdir -p "$dir_path"
-  cp "/tmp/input/$uri" "$dir_path/"
+  cp "$TMP/input/$uri" "$dir_path/"
 done
 
-$TIDDLYWIKI +plugins/tiddlywiki/markdown --verbose --load /tmp/input/phajas-wiki.html --render "[[$:/plugins/sq/feeds/templates/rss]]" "rss.xml" "text/plain" "$:/core/templates/wikified-tiddler"
+$TIDDLYWIKI +plugins/tiddlywiki/markdown --load $TMP/input/phajas-wiki.html --render "[[$:/plugins/sq/feeds/templates/rss]]" "rss.xml" "text/plain" "$:/core/templates/wikified-tiddler"
+
 mv output/rss.xml $OUTPUT/rss.xml
 
-find /tmp/NEW -type f | sed 's/tmp\/NEW/out/' | sort > /out/manifest.txt
+rm -r $BASE
 
-rm -r /out/BASE
+find "$OUTPUT" | sed 's/NEW\///' | sort > $OUT/wiki_manifest.txt
 
-mv --force /tmp/NEW/* /out/
+mv $OUTPUT/* $OUT/
+
+rm -r $OUTPUT
 
