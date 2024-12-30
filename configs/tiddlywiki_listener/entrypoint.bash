43c3366482aeabb40b3bffaf4f93f39b6540c0ce
Peter Hajas <peter@haj.as>
2024-12-30 06:49:48 -0700

    more webpage fixes:
    * use correct paths for scripts
    * clean manifest as late as possible

--- configs/tiddlywiki_listener/entrypoint.bash
+++ configs/tiddlywiki_listener/entrypoint.bash
@@ -11,9 +11,11 @@ TMP=/tmp
 OUT=/out
 WIKI_SRC=/wiki
 
+# ---
+
 TIDDLYWIKI="tiddlywiki"
 
-OUTPUT=$OUT/NEW
+OUTPUT=$TMP/NEW
 BASE=$OUT/BASE
 
 rm -r $BASE 2>>/dev/null
@@ -23,26 +25,10 @@ mkdir -p $OUTPUT
 mkdir -p $BASE
 git --git-dir="$WIKI_SRC" archive master | tar -x -C $TMP/input/
 
-# clean up old manifest
-while IFS= read -r filepath
-do
-    # Trim whitespace and expand any potential wildcards
-    filepath=$(echo "$filepath" | xargs)
-    
-    # Skip empty lines
-    if [ -z "$filepath" ]; then
-        continue
-    fi
-    
-    # Remove file, count failures
-    rm "$filepath"
-    rm -r "$filepath"
-done < "$OUT/wiki_manifest.txt"
-
 $TIDDLYWIKI --load $TMP/input/phajas-wiki.html --output $TMP/ --render '.' public.json 'text/plain' '$:/core/templates/exporters/JsonFile' 'exportFilter' '[tag[Public]]:or[tag[phajas]]:or[prefix[$:/phajas]]:except[tag[Private]]' +plugins/tiddlywiki/markdown
 
-./tiddlywiki_strip_public_tag $TMP/public.json
-./tiddlywiki_apply_public_fields $TMP/public.json
+/tiddlywiki_strip_public_tag $TMP/public.json
+/tiddlywiki_apply_public_fields $TMP/public.json
 
 $TIDDLYWIKI $BASE --init empty 
 $TIDDLYWIKI +plugins/tiddlywiki/markdown $BASE --build index
@@ -75,11 +61,27 @@ done
 
 $TIDDLYWIKI +plugins/tiddlywiki/markdown --load $TMP/input/phajas-wiki.html --render "[[$:/plugins/sq/feeds/templates/rss]]" "rss.xml" "text/plain" "$:/core/templates/wikified-tiddler"
 
+# clean up old manifest
+while IFS= read -r filepath
+do
+    # Trim whitespace and expand any potential wildcards
+    filepath=$(echo "$filepath" | xargs)
+    
+    # Skip empty lines
+    if [ -z "$filepath" ]; then
+        continue
+    fi
+    
+    # Remove file
+    rm "$filepath"
+    rm -r "$filepath"
+done < "$OUT/wiki_manifest.txt"
+
 mv output/rss.xml $OUTPUT/rss.xml
 
 rm -r $BASE
 
-find "$OUTPUT" | sed 's/NEW\///' | sort > $OUT/wiki_manifest.txt
+find "$OUTPUT" | sed 's/NEW\///' | sed 's/tmp/out/' | sort > $OUT/wiki_manifest.txt
 
 mv $OUTPUT/* $OUT/
 
